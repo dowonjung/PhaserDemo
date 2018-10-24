b5ca750bdcfc44ac9055eb45949d30989f1fed58
dowonjung <dowonjung@utexas.edu>
2018-10-24 00:57:41 -0500

    repository changed

---
+++ state5.js
@@ -0,0 +1,10 @@
+demo.state5 = function(){};
+demo.state5.prototype = {
+    preload: function(){},
+    create: function(){
+        game.stage.backgroundColor = '#80ff80';
+        console.log('state5');
+        addChangeStateEventListeners();
+    },
+    update: function(){}
+};
